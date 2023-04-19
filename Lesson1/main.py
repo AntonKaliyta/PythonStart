13fb11a966f8d7d17a7cdcf6831ef1339e3f10e4
«anton» <kaluta99@gmail.com>
2023-04-19 19:15:54 +0700

    начал

---
+++ Lesson1/main.py
@@ -0,0 +1,20 @@
+# print('Введите первое число')
+# x = input()
+# xint = int(x)
+
+# y = input('Введите второе число ')
+# yint = int(y)
+# print(x, " + ", y, ' = ', xint+yint)
+
+# 
+
+# a = 6.3432
+# b = 3.342
+# x = a*b
+
+# print(round(x,2))
+
+# 
+# i = 3
+# i **=3
+# print(i)
