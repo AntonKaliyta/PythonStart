78f16b92de1b587aeae6ea00456b04599bac457a
«anton» <kaluta99@gmail.com>
2023-05-13 04:41:40 +0700

    прорешено

---
+++ Homework5/task26.py
@@ -0,0 +1,12 @@
+# программа принимает два числа и возводит А в целую степень В с помощью рекурсии
+
+def pow(num, power):
+    if (power == 1):
+        return num
+    else:
+        return num * pow(a, power-1)
+
+a = int(input('Введите число А '))
+b = int(input('Введите число B '))
+
+print(pow(a,b))
