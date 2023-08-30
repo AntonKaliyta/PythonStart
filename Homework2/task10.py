d7f974d15cb3cb320f9cbed4e75298d5cbc8a5ba
«anton» <kaluta99@gmail.com>
2023-08-30 19:04:37 +0700

    подправлено

--- Homework2/task10.py
+++ Homework2/task10.py
@@ -2,33 +2,33 @@
 # Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть
 
-n = int(input('Введите кол-во моент '))
-upCount = 0
-downCount = 0
-for i in range(n):
-    x = input('Укажите, какой сторой вверх лежит следующая монета ')
-    if x == 'герб' or x == 'гербом':
-        upCount+=1
-    elif x == 'решка' or x == 'решкой':
-        downCount+=1
-min = downCount
-if upCount<min:
-    min = upCount
-print(f'Кол-во монет которые необходимо перевернуть равно {downCount}')
-
-# или
-
 # n = int(input('Введите кол-во моент '))
 # upCount = 0
 # downCount = 0
 # for i in range(n):
-#     x = int(input('Укажите, какой сторой вверх лежит следующая монета(0 - решкой, 1 - гербом '))
-#     if x == 0:
-#         upCount += 1
-#     elif x == 1:
-#         downCount += 1
+#     x = input('Укажите, какой сторой вверх лежит следующая монета ')
+#     if x == 'герб' or x == 'гербом':
+#         upCount+=1
+#     elif x == 'решка' or x == 'решкой':
+#         downCount+=1
 # min = downCount
-# if upCount < min:
+# if upCount<min:
 #     min = upCount
-# print(f'Кол-во монет которые необходимо перевернуть равно {min}')
+# print(f'Кол-во монет которые необходимо перевернуть равно {downCount}')
+
+# или
+
+n = int(input('Введите кол-во моент '))
+upCount = 0
+downCount = 0
+for i in range(n):
+    x = int(input('Укажите, какой сторой вверх лежит следующая монета(0 - решкой, 1 - гербом '))
+    if x == 0:
+        upCount += 1
+    elif x == 1:
+        downCount += 1
+min = downCount
+if upCount < min:
+    min = upCount
+print(f'Кол-во монет которые необходимо перевернуть равно {min}')
 
