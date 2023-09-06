aa12a9c70fb6774d2b4e949e7a2ea083d8cddb0f
AntonKaliyta <kaluta99@gmail.com>
2023-09-07 00:39:50 +0700

    тест коммит

--- Seminar3/2.py
+++ Seminar3/2.py
@@ -1,12 +1,14 @@
-# k = int(input('Введите положительное число  '))
+k = int(input('Введите положительное число  '))
 
-# list_1 = [1,2,3,4,5]
+list_1 = [1,2,3,4,5]
 
-# # for i in range(k):
-# #     list_1.insert(0,list_1[-1])
-# #     list_1.pop(-1)
-    
 # for i in range(k):
-#     list_1.insert(0, list_1.pop())    # можно даже так сократить, т.к. pop возвращает и удаляет значение
+#     list_1.insert(0,list_1[-1])
+#     list_1.pop(-1)
+    
+for i in range(k):
+    list_1.insert(0, list_1.pop())    # можно даже так сократить, т.к. pop возвращает и удаляет значение
 
-# print(list_1)
+print(list_1)
+
+# послание
