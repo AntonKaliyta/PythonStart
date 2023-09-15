387725acb4358c68697f1918e1705d046058a515
AntonKaliyta <kaluta99@gmail.com>
2023-09-15 16:49:40 +0700

    добавил вариант с choices

--- Homework3/task16.py
+++ Homework3/task16.py
@@ -2,20 +2,29 @@
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
 # В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
 
-arrayA = []
-n = int(input('Введите кол-во элементов в массиве: '))
+# arrayA = []
+# n = int(input('Введите кол-во элементов в массиве: '))
 
-arrayA = [i for i in range(1, n+1)]
+# arrayA = [i for i in range(1, n+1)]
+# print(arrayA)
+
+# x = int(input('Введите искомое число: '))
+
+# count = 0
+# for i in range(len(arrayA)):
+#     if i == x:
+#         count += 1  
+# print(count)
+
+
+
+from random import choices 
+
+num = int(input('Введите кол-во элементов в массиве: '))
+arrayA = choices(range(num * 2), k = num)
 print(arrayA)
 
-x = int(input('Введите искомое число: '))
-
-count = 0
-for i in range(len(arrayA)):
-    if i == x:
-        count += 1
-    
-print(count)
-
+result = arrayA.count(int(input('Введите искомое число: ')))
+print(result)
 
 
