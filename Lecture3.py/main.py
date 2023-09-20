0924c7d94d8082fc6e05bf30dab0cd43b4ea14dc
AntonKaliyta <kaluta99@gmail.com>
2023-09-20 17:41:07 +0700

    добавил

---
+++ Lecture3.py/main.py
@@ -0,0 +1,31 @@
+
+# def sum_numbers(n):
+#     summa = 0
+#     for i in range(1, n+1):
+#         summa += i
+#     print(summa)
+
+# sum_numbers(7)
+
+# return возвращает значение
+# def sum_numbers(n):
+#     summa = 0
+#     for i in range(1, n+1):
+#         summa += i
+#     return summa
+
+# print(sum_numbers(3))
+
+
+
+def sum_numbers(n, y = 'Hello'):
+    print(y)
+    summa = 0
+    for i in range(1, n+1):
+        summa += i
+    return summa
+
+print(sum_numbers(3))
+
+# Функция может быть любой сложности и возвращать любые объекты (списки, кортежи, и даже функции!):
+
