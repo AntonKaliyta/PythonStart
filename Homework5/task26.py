ffac7bc8f43e78523f7061f8cb98963b758a785c
AntonKaliyta <kaluta99@gmail.com>
2023-09-20 18:03:47 +0700

    прорешал

--- Homework5/task26.py
+++ Homework5/task26.py
@@ -1,12 +1,26 @@
 # программа принимает два числа и возводит А в целую степень В с помощью рекурсии
 
-def my_pow(num, power):
-    if (power == 1):
-        return num
-    else:
-        return num * pow(a, power-1)
+# def my_pow(num, power):
+#     if (power == 1):
+#         return num
+#     else:
+#         return num * pow(a, power-1)
 
-a = int(input('Введите число А '))
-b = int(input('Введите число B '))
+# a = int(input('Введите число А '))
+# b = int(input('Введите число B '))
 
-print(pow(a,b))
+# print(pow(a,b))
+
+
+
+
+
+
+# def num_sq(a,b):
+#     if b == 1:
+#         return a
+#     else:
+#         return a * num_sq(a,b-1)
+
+# num_a,num_b = int(input()),int(input())
+# print(num_sq(num_a,num_b))
